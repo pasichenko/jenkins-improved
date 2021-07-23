d0b71b95b366a185e7fc2e9584c5b657684a4aa2
Pasichenko Maksym <maks23814300@gmail.com>
2021-07-23 11:26:08 +0300

    Initial commit

---
+++ src/main/java/com/maksympasichenko/jenkinsdefault/JenkinsDefaultApplication.java
@@ -0,0 +1,13 @@
+package com.maksympasichenko.jenkinsdefault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDefaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDefaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maksympasichenko/jenkinsdefault/JenkinsDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksympasichenko.jenkinsdefault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDefaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
